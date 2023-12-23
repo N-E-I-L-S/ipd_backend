12db7c207ca03a6a4dca96e2fc34208e7d0100a5
Neil Shukla <neilpshukla221203@gmail.com>
2023-12-23 23:24:45 +0530

    port fixed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
 
-const port = 3001
+const port = process.env.PORT || 3030;
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
